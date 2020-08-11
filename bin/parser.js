03d6c68956b3f1108f5fd11a52bab2563e6ce3e5
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-08-11 10:05:07 +0200

    wip #85

--- bin/parser.js
+++ bin/parser.js
@@ -40,101 +40,123 @@ program.option('-w, --watch', 'watch for file changes');
 program.option('-e, --external <value>', 'external references (key=value, can be used multiple times', collect, []); // We support multiple uses of this option.
 program.parse(process.argv);
 
-if (!program.input) {
-  console.error('Please provide an input file using -i| --input.');
-} else {
-  let inputPaths = [];
+main();
 
-  for (let input of program.input) {
-    // Check if the input is a regex, e.g., *.yarrrml
-    if (glob.hasMagic(input)) {
-      const foundFiles = glob.sync(input).map(file => path.join(process.cwd(), file));
-      inputPaths = inputPaths.concat(foundFiles);
-    } else {
-      if (!path.isAbsolute(input)) {
-        input = path.join(process.cwd(), input);
-      }
+async function main() {
+  if (!program.input) {
+    console.error('Please provide an input file using -i| --input.');
+  } else {
+    let inputPaths = [];
 
-      inputPaths.push(input);
-    }
-  }
-
-  if (!program.watch) {
-    try {
-      const inputData = [];
-
-      for (const p of inputPaths) {
-        const yarrrml = fs.readFileSync(p, 'utf8');
-        inputData.push({yarrrml, file: p});
-      }
-
-      if (program.format) {
-        program.format = program.format.toUpperCase();
-      }
-
-      let triples;
-
-      let prefixes = {
-        rr: namespaces.rr,
-        rdf: namespaces.rdf,
-        rdfs: namespaces.rdfs,
-        fnml: "http://semweb.mmlab.be/ns/fnml#",
-        fno: "https://w3id.org/function/ontology#",
-        d2rq: "http://www.wiwiss.fu-berlin.de/suhl/bizer/D2RQ/0.1#"
-      };
-
-      const externalReferences = {};
-
-      for (const e of program.external) {
-        const keyValue = e.split('=');
-        externalReferences[keyValue[0]] = keyValue[1];
-      }
-
-      if (!program.format || program.format === 'RML') {
-        const y2r = new Y2R({class: !!program.class, externalReferences});
-        triples = y2r.convert(inputData);
-
-        prefixes.rml = namespaces.rml;
-        prefixes.ql = namespaces.ql;
-        prefixes[''] = y2r.getBaseIRI();
-        prefixes = Object.assign({}, prefixes, y2r.getPrefixes());
+    for (let input of program.input) {
+      // Check if the input is a regex, e.g., *.yarrrml
+      if (glob.hasMagic(input)) {
+        const foundFiles = glob.sync(input).map(file => path.join(process.cwd(), file));
+        inputPaths = inputPaths.concat(foundFiles);
       } else {
-        const y2r = new Y2R2({class: !!program.class, externalReferences});
-        triples = y2r.convert(inputData);
-        prefixes[''] = y2r.getBaseIRI();
-        prefixes = Object.assign({}, prefixes, y2r.getPrefixes());
+        if (!path.isAbsolute(input)) {
+          input = path.join(process.cwd(), input);
+        }
+
+        inputPaths.push(input);
       }
+    }
 
-      const writer = new N3.Writer({prefixes});
+    if (!program.watch) {
+      try {
+        const inputData = [];
 
-      writer.addQuads(triples);
-      writer.end((error, result) => {
-        if (program.output) {
-          if (!path.isAbsolute(program.output)) {
-            program.output = path.join(process.cwd(), program.output);
-          }
+        for (const p of inputPaths) {
+          const yarrrml = fs.readFileSync(p, 'utf8');
+          inputData.push({yarrrml, file: p});
+        }
+
+        if (program.format) {
+          program.format = program.format.toUpperCase();
+        }
+
+        let triples;
+
+        let prefixes = {
+          rr: namespaces.rr,
+          rdf: namespaces.rdf,
+          rdfs: namespaces.rdfs,
+          fnml: "http://semweb.mmlab.be/ns/fnml#",
+          fno: "https://w3id.org/function/ontology#",
+          d2rq: "http://www.wiwiss.fu-berlin.de/suhl/bizer/D2RQ/0.1#"
+        };
+
+        const externalReferences = {};
+
+        for (const e of program.external) {
+          const keyValue = e.split('=');
+          externalReferences[keyValue[0]] = keyValue[1];
+        }
+
+        if (!program.format || program.format === 'RML') {
+          const y2r = new Y2R({class: !!program.class, externalReferences});
 
           try {
-            fs.writeFileSync(program.output, result);
+            triples = await y2r.convert(inputData);
           } catch (e) {
-            console.error(`The RML could not be written to the output file ${program.output}`);
-          }
-        } else {
-          console.log(result);
-        }
-      });
+            if (e.code === 'INVALID_YARRRML') {
+              if (e.validationErrors.length === 1) {
+                console.error(`YARRRML document is invalid: ${e.validationErrors[0].message}.`);
+              } else {
+                console.error('YARRRML document is invalid:');
+                for (const err of e.validationErrors) {
+                  console.error(`\t - ${err.message}.`);
+                }
+              }
+            } else {
+              console.error(e);
+            }
 
-    } catch (e) {
-      if (e.code === 'ENOENT') {
-        console.error(`The input file ${program.input} is not found.`);
-      } else if (e.code === 'INVALID_YAML') {
-        console.error(`The input file contains invalid YAML.`);
-        console.error(`line ${e.parsedLine}: ${e.message}`);
-      } else {
-        console.error(e);
+            process.exit(1);
+          }
+
+          prefixes.rml = namespaces.rml;
+          prefixes.ql = namespaces.ql;
+          prefixes[''] = y2r.getBaseIRI();
+          prefixes = Object.assign({}, prefixes, y2r.getPrefixes());
+        } else {
+          const y2r = new Y2R2({class: !!program.class, externalReferences});
+          triples = y2r.convert(inputData);
+          prefixes[''] = y2r.getBaseIRI();
+          prefixes = Object.assign({}, prefixes, y2r.getPrefixes());
+        }
+
+        const writer = new N3.Writer({prefixes});
+
+        writer.addQuads(triples);
+        writer.end((error, result) => {
+          if (program.output) {
+            if (!path.isAbsolute(program.output)) {
+              program.output = path.join(process.cwd(), program.output);
+            }
+
+            try {
+              fs.writeFileSync(program.output, result);
+            } catch (e) {
+              console.error(`The RML could not be written to the output file ${program.output}`);
+            }
+          } else {
+            console.log(result);
+          }
+        });
+
+      } catch (e) {
+        if (e.code === 'ENOENT') {
+          console.error(`The input file ${program.input} is not found.`);
+        } else if (e.code === 'INVALID_YAML') {
+          console.error(`The input file contains invalid YAML.`);
+          console.error(`line ${e.parsedLine}: ${e.message}`);
+        } else {
+          console.error(e);
+        }
       }
+    } else {
+      watch(program.input, program.output, program.format);
     }
-  } else {
-    watch(program.input, program.output, program.format);
   }
 }
